c50428fba0925c4d83dad536402c9530d27801b4
JohnyMielony07 <jan.latoch@gmail.com>
2019-03-02 17:11:38 +0100

    add changing button's border radius

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const titleInput = document.getElementById('title');
 const textColorInput = document.getElementById('text-color');
 const backgroundColorInput = document.getElementById('bkg-color');
+const borderRadiusInput = document.getElementById('border-radius');
 
 const button = document.getElementById('button');
 
@@ -14,4 +15,8 @@ textColorInput.addEventListener('input', () => {
 
 backgroundColorInput.addEventListener('input', () => {
     button.style.backgroundColor = backgroundColorInput.value;
+})
+
+borderRadiusInput.addEventListener('input', () => {
+    button.style.borderRadius = borderRadiusInput.value + 'px';
 })
