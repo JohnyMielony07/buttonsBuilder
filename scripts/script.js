dd2e59a722ddd3a0da74713e298cdbd8518fe625
JohnyMielony07 <jan.latoch@gmail.com>
2019-03-02 17:07:54 +0100

    add changing button's background color

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 const titleInput = document.getElementById('title');
 const textColorInput = document.getElementById('text-color');
-const button = document.getElementById('button');
+const backgroundColorInput = document.getElementById('bkg-color');
 
+const button = document.getElementById('button');
 
 titleInput.addEventListener('input', () => {
     button.innerText = titleInput.value;
@@ -10,3 +11,7 @@ titleInput.addEventListener('input', () => {
 textColorInput.addEventListener('input', () => {
     button.style.color = textColorInput.value;
 });
+
+backgroundColorInput.addEventListener('input', () => {
+    button.style.backgroundColor = backgroundColorInput.value;
+})
